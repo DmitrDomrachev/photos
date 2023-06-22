763c88ca64556e75db662aed5d3de86eba0f2b0e
DmitrDomrachev <dmitr.domrachev@gmail.com>
2023-06-22 14:17:13 +0300

    Исправлено отсутсвие фона.

--- lib/ui/screen/photo_info/photo_info_page.dart
+++ lib/ui/screen/photo_info/photo_info_page.dart
@@ -17,58 +17,61 @@ class PhotoInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.vertical(bottom: Radius.circular(32)),
-              child: SizedBox(
-                height: size.height / 2.3,
-                width: size.width,
-                child: imageUrl == null
-                    ? const ColoredBox(
-                        color: Color.fromRGBO(50, 50, 50, 1),
-                      )
-                    : Image.network(
-                        imageUrl!,
-                        fit: BoxFit.cover,
-                      ),
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(bottom: Radius.circular(32)),
+                child: SizedBox(
+                  height: size.height / 2.3,
+                  width: size.width,
+                  child: imageUrl == null
+                      ? const ColoredBox(
+                          color: Color.fromRGBO(50, 50, 50, 1),
+                        )
+                      : Image.network(
+                          imageUrl!,
+                          fit: BoxFit.cover,
+                        ),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 21, horizontal: 27),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title ?? '',
-                    style: AppTypography.titleLarge,
-                  ),
-                  Text(
-                    '${likes ?? -1} likes',
-                    style: AppTypography.titleMedium,
-                  ),
-                ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 21, horizontal: 27),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title ?? '',
+                      style: AppTypography.titleLarge,
+                    ),
+                    Text(
+                      '${likes ?? -1} likes',
+                      style: AppTypography.titleMedium,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-        Positioned(
-          top: 43,
-          left: 15,
-          child: CupertinoButton(
-            padding: EdgeInsets.zero,
-            onPressed: () => context.goNamed('home'),
-            child: const Text(
-              '< Back',
-              style: AppTypography.appBarButton,
+            ],
+          ),
+          Positioned(
+            top: 43,
+            left: 15,
+            child: CupertinoButton(
+              padding: EdgeInsets.zero,
+              onPressed: () => context.goNamed('home'),
+              child: const Text(
+                '< Back',
+                style: AppTypography.appBarButton,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/ui/screen/photo_list/photo_list_page.dart
+++ lib/ui/screen/photo_list/photo_list_page.dart
@@ -13,34 +13,42 @@ class PhotoListPage extends ElementaryWidget<IPhotoPageWM> {
 
   @override
   Widget build(IPhotoPageWM wm) {
-    return CustomScrollView(
-      controller: wm.controller,
-      slivers: [
-        const CupertinoSliverNavigationBar(
-          largeTitle: Text(
-            'Photos',
-            style: AppTypography.appBarLargeTitle,
+    return CupertinoPageScaffold(
+      child: CustomScrollView(
+        controller: wm.controller,
+        slivers: [
+          const CupertinoSliverNavigationBar(
+            largeTitle: Text(
+              'Photos',
+              style: AppTypography.appBarLargeTitle,
+            ),
+            middle: Text(
+              'Photos',
+              style: AppTypography.appBarMiddle,
+            ),
+            alwaysShowMiddle: false,
+            backgroundColor: Color.fromRGBO(255, 255, 255, 0.75),
+            border:
+                Border(bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0))),
           ),
-          middle: Text(
-            'Photos',
-            style: AppTypography.appBarMiddle,
-          ),
-          alwaysShowMiddle: false,
-          backgroundColor: Color.fromRGBO(255, 255, 255, 0.75),
-          border: Border(bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0))),
-        ),
-        EntityStateNotifierBuilder(
+          EntityStateNotifierBuilder(
             listenableEntityState: wm.photos,
             errorBuilder: (_, __, data) {
-              return PhotoGridView(data: data, onPressed: wm.navigateToPhotoInfo,);
+              return PhotoGridView(
+                data: data,
+                onPressed: wm.navigateToPhotoInfo,
+              );
             },
             loadingBuilder: (_, data) {
-              return PhotoGridView(data: data, onPressed: wm.navigateToPhotoInfo);
+              return PhotoGridView(
+                  data: data, onPressed: wm.navigateToPhotoInfo);
             },
             builder: (_, data) {
-              return PhotoGridView(data: data, onPressed: wm.navigateToPhotoInfo);
-            },),
-        EntityStateNotifierBuilder(
+              return PhotoGridView(
+                  data: data, onPressed: wm.navigateToPhotoInfo);
+            },
+          ),
+          EntityStateNotifierBuilder(
             listenableEntityState: wm.photos,
             errorBuilder: (_, __, ___) {
               return const SliverToBoxAdapter();
@@ -50,8 +58,10 @@ class PhotoListPage extends ElementaryWidget<IPhotoPageWM> {
             },
             builder: (_, data) {
               return const SliverToBoxAdapter();
-            },),
-      ],
+            },
+          ),
+        ],
+      ),
     );
   }
 }
